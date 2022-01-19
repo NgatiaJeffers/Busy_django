5aa1b2a876c67c445311d8fe9ddbd90f73adb4e5
NgatiaJeffers <jeffersongakuya@gmail.com>
2022-01-19 18:04:45 +0300

    #Add URL patters for views

--- blog-app/blog/views.py
+++ blog-app/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,14 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html',
                 {'posts': posts})
+
+# Posts details
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                            status='published',
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    return render(request,
+                'blog/post/detail.html',
+                {'post':post})
